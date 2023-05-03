534886c1a497141b4811c14c19979c559f997696
Mykola Yurchenko <mykolayurchenko@Mykolas-MacBook-Pro.local>
2023-05-03 21:20:24 +0200

    Add env for gcp storage

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -9,6 +9,10 @@ export async function GET() {
     keyFilename: process.env.GCP_CREDENTIALS_PATH
   });
 
+  console.log(process.env.GCP_PROJECT)
+  console.log(process.env.GCP_CREDENTIALS_PATH)
+  console.log(process.env.GCP_STORAGE_NAME)
+
   try {
     const [files] = await storage.bucket(process.env.GCP_STORAGE_NAME).getFiles();
     return NextResponse.json({ data: files.map(f => f.name) })
